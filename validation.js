988c3a8708c80132368557e3d3e5d1956233b65e
mattyj-dev <mjuleson@gmail.com>
2023-03-13 18:26:07 -0700

    added console log script

--- validation.js
+++ validation.js
@@ -1,5 +1,13 @@
 // validation script here
 
+const inputs = document.querySelectorAll('input');
+
 const patterns = {
     telephone: /^\d{10}$/
 }
+
+inputs.forEach((input) => {
+    input.addEventListener('keyup', (e) => {
+        console.log(e.target.attributes.name.value);
+    })
+})
